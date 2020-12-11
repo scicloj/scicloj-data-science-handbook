040138ba2fa57f527418cf16107467e404866036
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-12-11 12:21:55 +0000

    notespace: add requires and functions for use

--- src/scicloj/data_science_handbook.clj
+++ src/scicloj/data_science_handbook.clj
@@ -1,7 +1,28 @@
 (ns scicloj.data-science-handbook
-  (:gen-class))
+  (:gen-class)
+  (:require [notespace.api :as notespace]
+            [notespace.kinds :as kind]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+;; Notespace
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Listen for changes in the namespace and update notespace automatically
+;; Hidden kinds should not show in the notespace page
+^kind/hidden
+(comment
+  ;; Manually start an empty notespace
+  (notespace/init-with-browser)
+
+  ;; Renders the notes and listens to file changes
+  (notespace/listen)
+
+  ;; Clear an existing notespace browser
+  (notespace/init)
+  )
+
+;; Notespace examples
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Full details at:
+;; https://scicloj.github.io/notespace/doc/notespace/v3-experiment1-test/index.html
+
+["Template for each chapter notespace, awesome"]
