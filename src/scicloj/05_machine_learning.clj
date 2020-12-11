48ceda9a748796cf7e566c83d127357290d385e8
daslu <daniel.slutsky@gmail.com>
2020-12-11 14:54:51 +0200

    added a beginning to Chapter 5

--- src/scicloj/05_machine_learning.clj
+++ src/scicloj/05_machine_learning.clj
@@ -1,6 +1,7 @@
 (ns scicloj.05-machine-learning
   (:require [notespace.api :as notespace]
-            [notespace.kinds :as kind]))
+            [notespace.kinds :as kind]
+            [clojure.java.io :as io]))
 
 ;; Notespace
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -16,13 +17,41 @@
 
   ;; Clear an existing notespace browser
   (notespace/init)
+
+  ;; Evaluating a whole notespace
+  (notespace/eval-this-notespace)
   )
 
 ;; Chapter 05 - Machine learning
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 
-["Chapter 05 - Machine Learning"]
-
+["# Chapter 05 - Machine Learning"]
 
 ["This chapter will dive into practical aspects of machine learning using the Clojure libraries and toold (TODO)"]
+
+["## Intro"
+
+ "Getting the Iris dataset"]
+
+(require '[clojure.java.io :as io]
+         '[tablecloth.api :as tablecloth]
+         '[scicloj.helpers :as helpers])
+
+(def iris-dataset
+  (-> "data/iris.csv"
+      io/resource
+      (.toString)
+      (tablecloth/dataset {:key-fn helpers/->tidy-name})))
+
+^kind/dataset
+iris-dataset
+
+["Looking into the Iris data visually"]
+
+(require '[tech.viz.vega :as viz])
+
+^kind/vega
+(-> iris-dataset
+    (tablecloth/rows :as-maps)
+    (viz/scatterplot :sepal-width :sepal-length))
