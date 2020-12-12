1f8b276b0d15ad361858436a40f17ae0592fd59c
daslu <daniel.slutsky@gmail.com>
2020-12-12 13:47:10 +0200

    progress with chapter 5 (machine learning): basic linear regression

--- src/scicloj/05_machine_learning.clj
+++ src/scicloj/05_machine_learning.clj
@@ -33,16 +33,18 @@ Some parts of it are based on code from the [Python Data Science Handbook](https
 
 ["## Looking into the Iris dataset"]
 
+(require '[scicloj.data :as data])
 ^kind/dataset
 data/iris
 
 ["Visualizing:"]
 
-
 (require '[clojure.java.io :as io]
          '[tablecloth.api :as tablecloth]
          '[tech.viz.vega :as viz]
          '[aerial.hanami.common :as hc]
+         '[aerial.hanami.templates :as ht]
+         '[aerial.hanami.core :as hmi]
          '[scicloj.data :as data]
          '[scicloj.helpers :as helpers]
          '[scicloj.helpers.vega :as helpers.vega])
@@ -60,3 +62,76 @@ data/iris
  :ROWS [:sepal-length :sepal-width :petal-length :petal-width],
  :COLUMNS [:sepal-length :sepal-width :petal-length :petal-width]
  :COLOR-FIELD :species)
+
+["TODO: Add histograms at the diagonal, as typical SPLOMs (scatter plot matrices) do. For Vega-Lite, it is an open issue: https://github.com/vega/vega-lite/issues/3294."]
+
+["## Linear regression"]
+
+(require '[tech.v3.datatype :as dtype]
+         '[tech.v3.datatype.functional :as dfn]
+         '[fastmath.random :as random])
+
+["Random data:"]
+
+^kind/hiccup-nocode
+[:p/code
+ "import matplotlib.pyplot as plt
+import numpy as np
+
+rng = np.random.RandomState(42)
+x = 10 * rng.rand(50)
+y = 2 * x - 1 + rng.randn(50)
+plt.scatter(x, y);
+"
+ {:language :python}]
+
+(def linear-data
+  (let [n 50
+        rng (random/rng :isaac 42)
+        x (-> (random/->seq rng n)
+              (dfn/* 10))
+        y (-> x
+              (dfn/* 2)
+              (dfn/- 1)
+              (dfn/+ (random/->seq rng n)))]
+    (tablecloth/dataset
+     {:x (vec x)
+      :y (vec y)})))
+
+
+^kind/dataset
+linear-data
+
+^kind/vega
+(-> linear-data
+    (tablecloth/rows :as-maps)
+    (viz/scatterplot :x :y))
+
+["Linear modeling of the data:"]
+
+(require '[tech.v3.libs.smile.regression :as regression]
+         '[tech.v3.dataset.modelling :as modelling]
+         '[tech.v3.ml :as ml])
+
+(def linear-model
+  (-> linear-data
+      (modelling/set-inference-target :y)
+      (ml/train {:model-type :smile.regression/ridge
+                 :lambda     0.00001})))
+
+["TODO: We should use OLD (ordinary least squares here), but the tech.ml API does not wrap it yet. Ridge regression with a small `lambda` gives a close approximation."]
+
+(ml/explain linear-model)
+
+(def predictions-for-dummy-data
+  (let [dummy-data (tablecloth/dataset {:x (range 11)})]
+    (tablecloth/append dummy-data
+                       (ml/predict dummy-data linear-model))))
+
+^kind/dataset
+predictions-for-dummy-data
+
+^kind/vega
+(hc/xform ht/line-chart
+          :VALDATA (-> predictions-for-dummy-data
+                       (tablecloth/rows :as-maps)))
