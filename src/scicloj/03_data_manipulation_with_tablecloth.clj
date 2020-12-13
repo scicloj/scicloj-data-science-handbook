f44ae9eb54d69869c49249239159d4c441430fa8
kimim <kimi.im@outlook.com>
2020-12-13 10:26:38 +0800

    more ch3 section 1 examples

--- src/scicloj/03_data_manipulation_with_tablecloth.clj
+++ src/scicloj/03_data_manipulation_with_tablecloth.clj
@@ -29,15 +29,79 @@
 
 ["In the previous chapter, we dove into detail on (...)
 
-In this chapter, we will focus on the mechanics of using datasets (?Series, DataFrame?), and related structures effectively. We will use examples drawn from real datasets where appropriate, but these examples are not necessarily the focus."]
+In this chapter, we will focus on the mechanics of using datasets (?Series,
+DataFrame?), and related structures effectively. We will use examples drawn from
+real datasets where appropriate, but these examples are not necessarily the
+focus."]
 
 ["## Installing and Using tablecloth"
 
- "Installation of ..."]
+ "Installation of tablecloth is just as easy as add it to your deps.edn:
+  :deps
+  {
+    scicloj/tablecloth {:mvn/version \"5.00-beta-5a\"}
+  }"]
 
-(require '[clojure.java.io :as io]
-         '[tablecloth.api :as tablecloth]
-         '[scicloj.helpers :as helpers])
+["Once tablecloth is added to your project deps, you can require it and check the version:"]
+
+(require '[tablecloth.api :as tablecloth])
+
+;; TODO: we cannot check verson of tablecloth now. which is easy in pandas: `pandas.__version__`
+
+;; TODO we need to define a shorter alias for tablecloth later.
+
+["## Introducing Tablecloth Objects"]
+
+["At the very basic level, Tablecloth dataset is using tech.ml.dataset as the
+base. It is in-memory columnwise database. As we will see during the course of
+this chapter, Tablecloth provides a host of useful tools, methods, and
+functionality on top of the basic data structures, but nearly everything that
+follows will require an understanding of what these structures are. Thus, before
+we go any further, let's introduce these two fundamental Tablecloth data
+structures: the Column and Dataset.
+
+We will start our code sessions with the Tablecloth require:"]
+
+(require '[tech.v3.dataset.column :as col])
+
+["### The tech.ml.dataset Column Object"]
+
+["A Column is a one-dimensional array of indexed data. It can be created from a
+vec or array as follows:"]
+
+(def column1 (col/new-column :a-column [0.25 0.5 0.75 1.0]))
+;; TODO: no notespace kind for column object at the moment
+column1
+
+(def column2 (col/new-column :a-column (float-array [0.25 0.5 0.75 1.0])))
+column2
+
+["As we see in the output, the Column wraps both a sequence of values, which we
+can access with the values and index attributes:"]
+
+(vec column1)
+
+(vec column2)
+
+["Column can be accessed by the index. Just as clojure vector, you can invoke
+column with index as the argument:"]
+
+(column1 1)
+
+["You can also select rows by seq of indices:"]
+
+(col/select column1 [1 2])
+
+(col/select column1 (range 1 3))
+
+
+
+["## The tech.ml.dataset"]
+
+(def data (tablecloth/dataset [0.25 0.5 0.75 1.0]))
+
+^kind/dataset
+data
 
 ["## Data Selection in Series"]
 
