e7c73aeeddcbe0d2e65e365895c305490ad829a2
Carsten Behring <carsten.behring@gmail.com>
2021-01-19 17:02:10 +0100

    move make_blob code to own ns

--- src/scicloj/05_08_random_forests.clj
+++ src/scicloj/05_08_random_forests.clj
@@ -12,7 +12,9 @@
          '[tech.viz.vega :as viz]
          '[tablecloth.api :as tablecloth]
          '[aerial.hanami.common :as hanami-common]
-         '[aerial.hanami.templates :as hanami-templates])
+         '[aerial.hanami.templates :as hanami-templates]
+         '[scicloj.helpers.datasets :as datasets]
+         )
 
 (comment
  ;; Manually start an empty notespaceff
@@ -24,71 +26,14 @@
  ;; Evaluating a whole notespace
  (notespace/eval-this-notespace))
 
-["Trying to mimic the make-blob function of sklearn (not sure about its implementation at the moment):"]
-
-["https://github.com/scikit-learn/scikit-learn/blob/42aff4e2e/sklearn/datasets/_samples_generator.py#L839"]
-(defn random-center
-  [rng]
-  (-> (repeatedly 2 #(random/drandom rng))
-      vec
-      (* 20)
-      (- 10)))
-
-(-> (random/rng :isaac 1337)
-    (random-center))
-
-(defn random-point
-  [rng]
-  (repeatedly 2 #(random/grandom rng)))
-
-(-> (random/rng :isaac 1337)
-    (random-point))
-
-(defn random-points-around-center
-  [rng n-samples center std]
-  (-> (repeatedly n-samples #(random-point rng))
-      tensor/->tensor
-      (* std)
-      (+ (tensor/broadcast center [n-samples 2]))))
-
-(let [rng    (random/rng :isaac 1337)
-      center [30 -40]
-      n-samples 100
-      std 10]
-  (-> (repeatedly n-samples #(random-point rng))
-      tensor/->tensor
-      (* std)
-      (+ (tensor/broadcast center [n-samples 2]))))
-
-(let [rng    (random/rng :isaac 1337)
-      center [30 -40]]
-  [center (random-points-around-center rng 12 center 3)])
-
-(defn make-blob ; mimicking the python make_blob function
-  [rng n-smaples n-centers std]
-  (->> #(random-center rng)
-       (repeatedly n-centers)
-       (map-indexed
-        (fn [i center]
-          (-> rng
-              (random-points-around-center (quot n-smaples
-                                                 n-centers)
-                                           center
-                                           std)
-              tensor/columns
-              (->> (map vec)
-                   (zipmap [:x :y]))
-              tablecloth/dataset
-              (tablecloth/add-or-replace-column :i i))))
-       (apply tablecloth/concat)))
 
 
 
-(let [rng (random/rng :isaac 1337)] (make-blob rng 10 2 1))
+
 
 ^kind/vega
 (-> (random/rng :isaac 1123)
-    (make-blob 300 4 1)
+    (datasets/make-blob 300 4 1)
     (tablecloth/rows :as-maps)
     (viz/scatterplot :x
                      :y
@@ -102,7 +47,7 @@
 
 (def original-dataset
   (-> (random/rng :isaac 1123)
-      (make-blob 300 4 1)))
+      (datasets/make-blob 300 4 1)))
 
 ^kind/dataset
 original-dataset
