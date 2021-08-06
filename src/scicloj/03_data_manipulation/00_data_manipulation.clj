fc13102f462453104e2a1426566607383dd6add4
kimim <kimi.im@outlook.com>
2021-08-06 22:33:51 +0800

    update document and display tablecloth version

--- src/scicloj/03_data_manipulation/00_data_manipulation.clj
+++ src/scicloj/03_data_manipulation/00_data_manipulation.clj
@@ -1,7 +1,8 @@
 (ns scicloj.03-data-manipulation.00-data-manipulation
   (:require [notespace.api :as notespace]
             [notespace.kinds :as kind]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [tablecloth.api :as tablecloth]))
 
 ;; Notespace
 ^kind/hidden
@@ -19,48 +20,51 @@
 
 ["# Data Manipulation with tablecloth"]
 
-["In the previous chapter, we dove into detail on NumPy and its ndarray object,
-which provides efficient storage and manipulation of dense typed arrays in
-Python. Here we'll build on this knowledge by looking in detail at the data
-structures provided by the Pandas library. Pandas is a newer package built on
-top of NumPy, and provides an efficient implementation of a
-DataFrame. DataFrames are essentially multidimensional arrays with attached row
-and column labels, and often with heterogeneous types and/or missing data. As
-well as offering a convenient storage interface for labeled data, Pandas
-implements a number of powerful data operations familiar to users of both
-database frameworks and spreadsheet programs.
+["In the previous chapter, we dove into detail on dataset, which provides
+efficient storage and manipulation of dense typed arrays in clojure. Here we'll
+build on this knowledge by looking in detail at the data structures provided by
+the dataset/tablecloth library. Tablecloth is a well defined API on top of
+dataset, and provides an efficient implementation of a dataset. Datasets are
+essentially multidimensional arrays with attached column labels, and often with
+heterogeneous types and/or missing data. As well as offering a convenient
+storage interface for labeled data, Tablecloth implements a number of powerful
+data operations familiar to users of both database frameworks and spreadsheet
+programs.
 
-As we saw, NumPy's ndarray data structure provides essential features for the
+As we saw, Clojure's vector data structure provides essential features for the
 type of clean, well-organized data typically seen in numerical computing
 tasks. While it serves this purpose very well, its limitations become clear when
 we need more flexibility (e.g., attaching labels to data, working with missing
 data, etc.) and when attempting operations that do not map well to element-wise
 broadcasting (e.g., groupings, pivots, etc.), each of which is an important
 piece of analyzing the less structured data available in many forms in the world
-around us. Pandas, and in particular its Series and DataFrame objects, builds on
-the NumPy array structure and provides efficient access to these sorts of \"data
+around us. Tablecloth, and in particular its Column and Dataset objects, builds on
+the clojure vector? structure and provides efficient access to these sorts of \"data
 munging\" tasks that occupy much of a data scientist's time.
 
-In this chapter, we will focus on the mechanics of using Series, DataFrame, and
+In this chapter, we will focus on the mechanics of using Columns, Dataset, and
 related structures effectively. We will use examples drawn from real datasets
 where appropriate, but these examples are not necessarily the focus."]
 
 ["## Installing and Using tablecloth"]
 
-["Installation of Pandas on your system requires NumPy to be installed, and if
-building the library from source, requires the appropriate tools to compile the
-C and Cython sources on which Pandas is built. Details on this installation can
-be found in the Pandas documentation. If you followed the advice outlined in the
-Preface and used the Anaconda stack, you already have Pandas installed."]
+["Installation of tablecloth on your system is as easy as just add the deps and
+require it in code."]
 
-["Once Pandas is installed, you can import it and check the version:"]
+["Once tablecloth is add to deps.edn, you can require it and check the
+version:"]
 
-(require 'tablecloth.api)
+^kind/naive
+(get-in (read-string (slurp "deps.edn"))
+        [:deps 'scicloj/tablecloth :mvn/version])
 
-;; TODO: How to check tablecloth version?
+;; TODO: shall we put tablecloth version info to API interface?
+;; for example: tablecloth.api/version
+;; even better that we can check upstream tech.ml.dataset version with:
+;; tablecloth.api/dataset-version
 
-["Just as we generally import NumPy under the alias np, we will import tablecloth
-under the alias tablecloth:"]
+["Just as we generally require clojure namespace under a alias, we will import
+tablecloth.api under the alias tablecloth:"]
 
 (require '[tablecloth.api :as tablecloth])
 
@@ -79,8 +83,8 @@ type:
 `tablecloth/<TAB>`"]
 
 ["And to display tablecloth's built-in documentation, you can use this:
-
-`(clojure.repl/doc tablecloth.api)`"]
+`(clojure.repl/doc tablecloth.api)` and switch to nrepl to check the
+printouts."]
 
 (clojure.repl/doc tablecloth.api)
 
