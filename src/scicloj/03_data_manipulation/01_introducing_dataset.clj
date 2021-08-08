68d6b1738f5b1601e02992d1508bea3e248d84ed
kimim <kimi.im@outlook.com>
2021-08-08 21:15:39 +0800

    create dataset from internet resource

--- src/scicloj/03_data_manipulation/01_introducing_dataset.clj
+++ src/scicloj/03_data_manipulation/01_introducing_dataset.clj
@@ -330,4 +330,32 @@ well:"]
 
 ^kind/dataset
 (tablecloth/dataset
- "https://github.com/scicloj/scicloj-data-science-handbook/blob/live/resources/data/iris.csv")
+ "https://raw.githubusercontent.com/scicloj/scicloj-data-science-handbook/live/resources/data/iris.csv")
+;; => https://raw.githubusercontent.com/scicloj/scicloj-data-science-handbook/live/resources/data/iris.csv [150 5]:
+;;    | Sepal.Length | Sepal.Width | Petal.Length | Petal.Width | Species |
+;;    |-------------:|------------:|-------------:|------------:|---------|
+;;    |          5.1 |         3.5 |          1.4 |         0.2 |  setosa |
+;;    |          4.9 |         3.0 |          1.4 |         0.2 |  setosa |
+;;    |          4.7 |         3.2 |          1.3 |         0.2 |  setosa |
+;;    |          4.6 |         3.1 |          1.5 |         0.2 |  setosa |
+;;    |          5.0 |         3.6 |          1.4 |         0.2 |  setosa |
+;;    |          5.4 |         3.9 |          1.7 |         0.4 |  setosa |
+;;    |          4.6 |         3.4 |          1.4 |         0.3 |  setosa |
+;;    |          5.0 |         3.4 |          1.5 |         0.2 |  setosa |
+;;    |          4.4 |         2.9 |          1.4 |         0.2 |  setosa |
+;;    |          4.9 |         3.1 |          1.5 |         0.1 |  setosa |
+;;    |          5.4 |         3.7 |          1.5 |         0.2 |  setosa |
+;;    |          4.8 |         3.4 |          1.6 |         0.2 |  setosa |
+;;    |          4.8 |         3.0 |          1.4 |         0.1 |  setosa |
+;;    |          4.3 |         3.0 |          1.1 |         0.1 |  setosa |
+;;    |          5.8 |         4.0 |          1.2 |         0.2 |  setosa |
+;;    |          5.7 |         4.4 |          1.5 |         0.4 |  setosa |
+;;    |          5.4 |         3.9 |          1.3 |         0.4 |  setosa |
+;;    |          5.1 |         3.5 |          1.4 |         0.3 |  setosa |
+;;    |          5.7 |         3.8 |          1.7 |         0.3 |  setosa |
+;;    |          5.1 |         3.8 |          1.5 |         0.3 |  setosa |
+;;    |          5.4 |         3.4 |          1.7 |         0.2 |  setosa |
+;;    |          5.1 |         3.7 |          1.5 |         0.4 |  setosa |
+;;    |          4.6 |         3.6 |          1.0 |         0.2 |  setosa |
+;;    |          5.1 |         3.3 |          1.7 |         0.5 |  setosa |
+;;    |          4.8 |         3.4 |          1.9 |         0.2 |  setosa |
